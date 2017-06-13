27a782091fbd82678d49be697d2c60b4ce2b1c41
Thomas Diesler <thomas.diesler@jboss.com>
2017-06-13 23:22:18 +0200

    Upgrade to projB-1.0.1 & projD-1.0.1

--- src/main/java/org/fuse/cidpoc/e/E.java
+++ src/main/java/org/fuse/cidpoc/e/E.java
@@ -11,12 +11,12 @@ public class E extends Item {
 
     @Override
     public Capability getCapability() {
-        return new Capability("E", 3);
+        return new Capability("E", 0);
     }
 
     @Override
     public List<Requirement> getRequirements() {
-        Requirement[] reqs = new Requirement[] { new Requirement("B", 3, 5), new Requirement("D", 3, 5) };
+        Requirement[] reqs = new Requirement[] { new Requirement("B", 5, 8), new Requirement("D", 5, 8) };
         return Arrays.asList(reqs);
     }
 
